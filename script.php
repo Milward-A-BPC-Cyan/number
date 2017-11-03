d3e2046cafbf616d2f8441da8c4eac34dc591e67
Milward-A-BPC-Cyan <32195117+Milward-A-BPC-Cyan@users.noreply.github.com>
2017-11-03 09:35:45 +0000

    Add files via upload

---
+++ script.php
@@ -0,0 +1,11 @@
+<?php
+$n = rand(1,6);
+echo "$n";
+if (n == 1) {
+	echo "Almost Snake Eyes";
+} elseif (n == 6) {
+	echo "Winner";
+} else {
+	echo "You didn't win, click the roll button to play again"
+}
+?>
