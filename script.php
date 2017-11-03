6c338c8f421df8f1c03c362f63e0532aa5032687
Milward-A-BPC-Cyan <32195117+Milward-A-BPC-Cyan@users.noreply.github.com>
2017-11-03 10:21:03 +0000

    Update script.php

--- script.php
+++ script.php
@@ -1,13 +1,15 @@
 <?php
 $n1 = rand(1,6);
 $n2 = rand(1,6);
-echo "$n1 \n"
-echo "$n2 \n"
+echo "$n1 \n";
+echo "$n2 \n";
 if ($n1 xor $n2 == 1) {
 	echo "Almost Snake Eyes";
 } elseif ($n1 && $n2 == 1) {
 	echo "Snake Eyes!";
 } elseif ($n1 && $n2 == 6) {
 	echo "DA TING GO SKKRRRRRRAT";
+} else {
+	echo "You lose";
 }
 ?>
