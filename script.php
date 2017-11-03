8b7aa2343005348fcc0629699f0d35e2a92c8d32
Milward-A-BPC-Cyan <32195117+Milward-A-BPC-Cyan@users.noreply.github.com>
2017-11-03 10:15:57 +0000

    Update script.php

--- script.php
+++ script.php
@@ -1,12 +1,13 @@
 <?php
-$n = rand(1,6);
-echo "$n \n";
-if ($n == 1) {
+$n1 = rand(1,6);
+$n2 = rand(1,6);
+echo "$n1 \n"
+echo "$n2 \n"
+if ($n1 xor $n2 == 1) {
 	echo "Almost Snake Eyes";
-} elseif ($n == 6) {
-	echo "Winner";
-} 
-else {
-	echo "You didn't win, click the roll button to play again";
+} elseif ($n1 && $n2 == 1) {
+	echo "Snake Eyes!";
+} elseif ($n1 && $n2 == 6) {
+	echo "DA TING GO SKKRRRRRRAT";
 }
 ?>
